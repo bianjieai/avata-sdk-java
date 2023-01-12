3b3b0ad74c66dbb8738738d779fce0b954bfd781
sxj <a550374@163.com>
2023-01-12 17:16:12 +0800

    add order and records

--- src/main/java/proxy/order/OrderProxy.java
+++ src/main/java/proxy/order/OrderProxy.java
@@ -4,11 +4,27 @@ import model.PublicResponse;
 import model.order.*;
 
 public interface OrderProxy {
+    /**
+     * 购买能量值/业务费
+     * @return
+     */
     PublicOrderRes CreatrOrder(CreateOrderReq req);
 
+    /**
+     * 批量购买能量值
+     * @return
+     */
     PublicOrderRes BatchCreatrOrders(BatchCreateOrdersReq req);
 
+    /**
+     * 查询能量值/业务费购买结果
+     * @return
+     */
     QueryOrderRes QueryOrder(QueryOrderReq req, String orderId);
 
+    /**
+     * 查询能量值/业务费购买结果列表
+     * @return
+     */
     QueryOrdersRes QueryOrders(QueryOrdersReq req);
 }
--- src/main/java/proxy/records/RecordsProxy.java
+++ src/main/java/proxy/records/RecordsProxy.java
@@ -4,5 +4,9 @@ import model.PublicResponse;
 import model.records.CreateRecordReq;
 
 public interface RecordsProxy {
+    /**
+     * 数字作品存证接口, 创建数字作品存证
+     * @return
+     */
     PublicResponse createRecord(CreateRecordReq req);
 }
