f632505e54ef8254965c54fef8d127a78d0ac53d
minghui <1229660539@qq.com>
2023-07-12 16:11:34 +0800

    add read_only in QueryAccountsRes response

--- src/main/java/ai/bianjie/avatasdk/model/account/QueryAccountsRes.java
+++ src/main/java/ai/bianjie/avatasdk/model/account/QueryAccountsRes.java
@@ -50,6 +50,11 @@ public class QueryAccountsRes {
 
             @JSONField(name = "operation_id")
             private String operationId;// 操作 ID
+
+            @JSONField(name = "read_only")
+            private String readOnly;// 如果使用【钱包应用】对应的项目参数调用该查询链账户接口时，会返回该字段。该字段用于区分某一指定链账户地址是否属于当前项目参数所对应的钱包应用。
+            //0: 由当前钱包创建，代表对某一指定地址拥有操作权限
+            //1: 由其他钱包创建，代表对某一指定地址只有只读权限，没有操作权限
         }
     }
 }
