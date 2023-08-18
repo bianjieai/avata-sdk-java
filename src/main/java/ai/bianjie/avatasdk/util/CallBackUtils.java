31059ccf72d0ad0a7a2c1a9bdd36957a9996ac41
minghui <1229660539@qq.com>
2023-08-18 16:17:40 +0800

    update callBack

--- src/main/java/ai/bianjie/avatasdk/util/CallBackUtils.java
+++ src/main/java/ai/bianjie/avatasdk/util/CallBackUtils.java
@@ -12,26 +12,25 @@ import static ai.bianjie.avatasdk.util.AvataUtils.sign;
 public class CallBackUtils {
 
     // 如果对接的是 v1 版本，用以下方式验签
-    public static String callBackV1(String body, String signature) {
+    public static String callBackV1(String body, String signature,String apiSecret,business func) {
         JSONObject jsonStr = JSON.parseObject(body);
         String jsonStr1 = JSON.toJSONString(jsonStr, SerializerFeature.MapSortField);
         // 执行签名
-        String signature1 = sha256Sum(jsonStr1 + "api_secret");
+        String signature1 = sha256Sum(jsonStr1 + apiSecret);
         if (signature1.equals(signature)) {
             //将body存储到本地
-            //TODO
+            func.businessFunction();
             return "SUCCESS";
         } else
             return "FAILED";
     }
 
-
-    // 如果对接的是 v2 或者 v3 版本，用以下方式验签
-    public static String callBack(String path, String body,Long timeStamp,String apiSecret,String signature) {
+    // 如果对接的是 v2 以其以上版本版本，用以下方式验签
+    public static String callBack(String path, String body,Long timeStamp,String apiSecret,String signature,business func) {
         String signature2 = sign(path, null, getMap(body), timeStamp, apiSecret);
         if (signature2.equals(signature)) {
             //将body存储到本地
-            //TODO
+            func.businessFunction();
             return "SUCCESS";
         } else
             return "FAILED";
@@ -41,4 +40,12 @@ public class CallBackUtils {
         Map<String, Object> maps = JSON.parseObject(canshu);
         return maps;
     }
+
+    /**
+     * 业务接口，应用方需要实现业务逻辑，在验签成功后执行
+     */
+    public  interface business{
+        void businessFunction();
+    }
 }
+
