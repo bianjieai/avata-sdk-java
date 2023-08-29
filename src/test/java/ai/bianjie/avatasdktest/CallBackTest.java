5c0b31b22bec06866a7f0f3508f6b68b4c29cb42
minghui <1229660539@qq.com>
2023-08-29 10:23:27 +0800

    update CallBackTest

--- src/test/java/ai/bianjie/avatasdktest/CallBackTest.java
+++ src/test/java/ai/bianjie/avatasdktest/CallBackTest.java
@@ -8,13 +8,12 @@ public class CallBackTest {
     @Test
     public String callBack() {
         // 应用方业务逻辑请写在 businessFunction() 匿名内部类中
-        CallBackUtils.callBackV1("", "", "", new CallBackUtils.business() {
+        return CallBackUtils.callBackV1("", "", "", new CallBackUtils.business() {
             @Override
             public void businessFunction() {
                 System.out.println("");
             }
         });
-        return "SUCCESS";
     }
 
 }
