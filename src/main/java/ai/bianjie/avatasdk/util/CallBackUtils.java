31eb09fc285dd0220eb5ce26b9a9e3bd0b85df3b
minghui <1229660539@qq.com>
2023-09-05 16:58:14 +0800

    回调优化

--- src/main/java/ai/bianjie/avatasdk/util/CallBackUtils.java
+++ src/main/java/ai/bianjie/avatasdk/util/CallBackUtils.java
@@ -1,5 +1,6 @@
 package ai.bianjie.avatasdk.util;
 
+import ai.bianjie.avatasdk.exception.AvataException;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
@@ -11,46 +12,46 @@ import static ai.bianjie.avatasdk.util.AvataUtils.sign;
 
 public class CallBackUtils {
 
+    // 验证签名时传入的 API 版本号
+    public static final String APIVersionV1 = "v1"; // v1 版本 AVATA Open API
+    public static final String APIVersionsOther = "v3"; // 其它版本 AVATA Open API,如 v2、v3
+
+
     /**
-     * 如果对接的是 v1 版本，用以下方式验签
+     * CallBackV1 v1 版本签名回调验签
      *
      * @param body      来自 Avata API 推送的请求体参数
      * @param signature 来自 Avata API 推送的请求头的签名，算法为 SHA256 (body + ApiSecret)
      * @param apiSecret 项目 API Secret
-     * @param app       应用方业务接口
      * @return
      */
-    public static String callBackV1(String body, String signature, String apiSecret, APP app) {
+    public static boolean callBackV1(String body, String signature, String apiSecret) {
         JSONObject jsonObj = JSON.parseObject(body);
         String jsonStr = JSON.toJSONString(jsonObj, SerializerFeature.MapSortField);
         // 执行签名
         String hexHash = sha256Sum(jsonStr + apiSecret);
-        if (hexHash.equals(signature)) {
-            app.onCallbackV1(body, signature, apiSecret);
-            return "SUCCESS";
-        } else
-            return "FAILED";
+        if (!hexHash.equals(signature)) {
+            return false;
+        }
+        return true;
     }
 
     /**
-     * 如果对接的是 v2 及其以上版本，用以下方式验签
+     * CallBack  v2 及其以上版本签名回调验签
      *
      * @param path      在 Avata服务平台设置的回调地址(去掉域名)
      * @param body      来自 Avata API 推送的请求体参数
      * @param timeStamp 来自 Avata API 推送的请求头中的时间戳
      * @param apiSecret 项目 API Secret
      * @param signature 来自 Avata API 推送的请求头的签名，与 网关鉴权签名 方式保持一致
-     * @param app       应用方业务接口
      * @return
      */
-
-    public static String callBack(String path, String body, Long timeStamp, String apiSecret, String signature, APP app) {
+    public static boolean callBack(String path, String body, Long timeStamp, String apiSecret, String signature) {
         String hexHash = sign(path, null, getMap(body), timeStamp, apiSecret);
-        if (hexHash.equals(signature)) {
-            app.onCallback(path, body, timeStamp, apiSecret, signature);
-            return "SUCCESS";
-        } else
-            return "FAILED";
+        if (!hexHash.equals(signature)) {
+            return false;
+        }
+        return true;
     }
 
     public static Map<String, Object> getMap(String params) {
@@ -59,14 +60,51 @@ public class CallBackUtils {
     }
 
     /**
+     * 接收来自 Avata 的推送消息
+     *
+     * @param version   版本 ：v1 需要传 APIVersionV1 , v2 或者 v3 版本传 APIVersionsOther
+     * @param body      来自 Avata API 推送的请求体参数
+     * @param timeStamp 来自 Avata API 推送的请求头中的时间戳
+     * @param apiSecret 项目 API Secret
+     * @param signature 来自 Avata API 推送的请求头的签名，与 网关鉴权签名 方式保持一致
+     * @param path      在 Avata服务平台设置的回调地址(去掉域名)
+     * @param app       自己的业务逻辑代码（验证签名通过才会执行）
+     * @return
+     */
+    public static String onCallback(String version, String body, Long timeStamp, String apiSecret, String signature, String path, APP app) {
+        boolean result = true;
+        switch (version) {
+            case APIVersionV1:
+                result = callBackV1(body, signature, apiSecret);
+                app.appV1(body, signature, apiSecret);   // 该笔推送消息属于文昌链上链完成所推送消息，请及时存储数据
+                break;
+            case APIVersionsOther:
+                result = callBack(path, body, timeStamp, apiSecret, signature);
+                app.app(path, body, timeStamp, apiSecret, signature);   // 该笔推送消息属于文昌链上链完成所推送消息，请及时存储数据
+                break;
+            default:
+                throw AvataException.NewSDKException("version verification failed");
+        }
+        if (!result) {
+            throw AvataException.NewSDKException("signature verification failed");
+        }
+
+        // 返回给消息推送端
+        return "SUCCESS";
+    }
+
+
+    /**
      * 业务接口，应用方需要实现业务逻辑，在验签成功后执行
      */
     public interface APP {
 
         // 如果对接的是 v1 版本，实现以下业务逻辑
-        void onCallbackV1(String body, String signature, String apiSecret);
+        void appV1(String body, String signature, String apiSecret);
 
         // 如果对接的是 v2 及其以上版本，实现以下业务逻辑
-        void onCallback(String path, String body, Long timeStamp, String apiSecret, String signature);
+        void app(String path, String body, Long timeStamp, String apiSecret, String signature);
     }
 }
+
+
--- src/test/java/ai/bianjie/avatasdktest/CallBackTest.java
+++ src/test/java/ai/bianjie/avatasdktest/CallBackTest.java
@@ -3,54 +3,43 @@ package ai.bianjie.avatasdktest;
 import ai.bianjie.avatasdk.util.CallBackUtils;
 import org.junit.jupiter.api.Test;
 
+import static ai.bianjie.avatasdk.util.CallBackUtils.APIVersionV1;
+
 public class CallBackTest {
     @Test
-    // 测试 V1 版本回调
-    public void callBackV1() {
-        String body = "Avata 推送的消息 body";
-        String signature = "Avata 推送的消息的签名";
-        String apiSecret = "API 项目 Secret";
-        // 实现业务接口
-        APP app = new APP();
-        String res = CallBackUtils.callBackV1(body, signature, apiSecret, app);
-        System.out.println(res);
-    }
 
-
-    @Test
-    // 测试 V2 以其以上版本回调
     public void callBack() {
-        String path = "回调地址（去掉域名）";
+        //测试 V1 版本回调
         String body = "Avata 推送的消息 body";
-        Long timeStamp = 1693387882000L;// Avata 推送的消息时间戳
         String signature = "Avata 推送的消息的签名";
         String apiSecret = "API 项目 Secret";
-        // 实现业务接口
-        APP app = new APP();
-        String res = CallBackUtils.callBack(path, body, timeStamp, apiSecret, signature, app);
+
+        // 测试 V2 以其以上版本回调
+        //String path = "回调地址（去掉域名）";
+        //String body = "Avata 推送的消息 body";
+        //Long timeStamp = 1693387882000L;// Avata 推送的消息时间戳
+        //String signature = "Avata 推送的消息的签名";
+        //String apiSecret = "API 项目 Secret";
+        String res = CallBackUtils.onCallback(APIVersionV1, body, 0L, apiSecret, signature, "", new CallBackUtils.APP() {
+            @Override
+            public void appV1(String body, String signature, String apiSecret) {
+                System.out.println("在此实现业务逻辑");
+                System.out.println(body);
+                System.out.println(apiSecret);
+                System.out.println(signature);
+            }
+
+            @Override
+            public void app(String path, String body, Long timeStamp, String apiSecret, String signature) {
+                System.out.println("在此实现业务逻辑");
+                System.out.println(path);
+                System.out.println(body);
+                System.out.println(timeStamp);
+                System.out.println(apiSecret);
+                System.out.println(signature);
+            }
+        });
         System.out.println(res);
     }
 }
 
-// 实现业务逻辑类
-class APP implements CallBackUtils.APP {
-    @Override
-    public void onCallbackV1(String body, String signature, String apiSecret) {
-        // 在此实现业务逻辑
-        System.out.println("在此实现业务逻辑");
-        System.out.println(body);
-        System.out.println(signature);
-        System.out.println(apiSecret);
-    }
-
-    @Override
-    public void onCallback(String path, String body, Long timeStamp, String apiSecret, String signature) {
-        // 在此实现业务逻辑
-        System.out.println("在此实现业务逻辑");
-        System.out.println(path);
-        System.out.println(body);
-        System.out.println(timeStamp);
-        System.out.println(apiSecret);
-        System.out.println(signature);
-    }
-}
