93c1252bd4a36329703b1ddd57128e2568375324
minghui <1229660539@qq.com>
2023-09-13 17:38:19 +0800

    新增 onCallbackRes

---
+++ src/main/java/ai/bianjie/avatasdk/model/onCallbackRes.java
@@ -0,0 +1,186 @@
+package ai.bianjie.avatasdk.model;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class onCallbackRes {
+
+    // 验证签名时传入的 API 版本号
+    public static final String APIVersionV1 = "V1"; // V1 版本 AVATA Open API
+    public static final String APIVersionsOther = "";// 其它版本 AVATA Open API
+
+    // 区分调用不同服务模块的接口
+    public String Native = "native";
+    public String EVM = "evm";
+
+    // OnCallbackResV1 AVATA Open API V1 版本回调服务参数
+    @Data
+    @NoArgsConstructor
+    public class onCallbackResV1 {
+
+        @JSONField(name = "operation_id")
+        private String operationId;// 操作 ID
+
+        @JSONField(name = "module")
+        private String module;// 交易模块
+
+        @JSONField(name = "type")
+        private String type;// 用户操作类型
+
+        @JSONField(name = "status")
+        private Integer status;// 交易状态，1 成功; 2 失败
+
+        @JSONField(name = "tx_hash")
+        private String txHash;// 交易哈希
+
+        @JSONField(name = "message")
+        private String message;// 交易失败的错误描述信息
+
+        @JSONField(name = "block_height")
+        private Integer blockHeight;// 交易上链的区块高度
+
+        @JSONField(name = "timestamp")
+        private String timestamp;// 交易上链时间（UTC 时间）
+
+        @JSONField(name = "nft")
+        private NftV1 nft;//对应不同操作类型的消息体(json)
+
+        @JSONField(name = "mt")
+        private Mt mt;// 对应不同操作类型的消息体(json)
+
+        @JSONField(name = "record")
+        private Record record;// 对应不同操作类型的消息体(json)
+    }
+
+
+    @Data
+    @NoArgsConstructor
+    public class onCallbackResNative {
+
+        @JSONField(name = "kind")
+        private Kind kind;// 区分服务，native / evm
+
+        @JSONField(name = "operation_id")
+        private String operationId;// 操作 ID
+
+        @JSONField(name = "module")
+        private Integer module;// 交易模块
+
+        @JSONField(name = "operation")
+        private Integer operation;// 用户操作类型
+
+        @JSONField(name = "tx_hash")
+        private String txHash;// 交易哈希
+
+        @JSONField(name = "status")
+        private Integer status;// 交易状态，1 成功; 2 失败
+
+        @JSONField(name = "message")
+        private String message;// 交易失败的错误描述信息
+
+        @JSONField(name = "block_height")
+        private Integer blockHeight;// 交易上链的区块高度
+
+        @JSONField(name = "timestamp")
+        private String timestamp;// 交易上链时间（UTC 时间）
+
+        @JSONField(name = "nft")
+        private NftNative nft;//对应不同操作类型的消息体(json)
+
+        @JSONField(name = "mt")
+        private Mt mt;// 对应不同操作类型的消息体(json)
+
+        @JSONField(name = "record")
+        private Record record;// 对应不同操作类型的消息体(json)
+    }
+
+
+    @Data
+    @NoArgsConstructor
+    public class onCallbackResEVM {
+
+        @JSONField(name = "kind")
+        private Kind kind;// 区分服务，native / evm
+
+        @JSONField(name = "operation_id")
+        private String operationId;// 操作 ID
+
+        @JSONField(name = "module")
+        private Integer module;// 交易模块
+
+        @JSONField(name = "operation")
+        private Integer operation;// 用户操作类型
+
+        @JSONField(name = "tx_hash")
+        private String txHash;// 交易哈希
+
+        @JSONField(name = "status")
+        private Integer status;// 交易状态，1 成功; 2 失败
+
+        @JSONField(name = "message")
+        private String message;// 交易失败的错误描述信息
+
+        @JSONField(name = "block_height")
+        private Integer blockHeight;// 交易上链的区块高度
+
+        @JSONField(name = "timestamp")
+        private String timestamp;// 交易上链时间（UTC 时间）
+
+        @JSONField(name = "nft")
+        private NftEVM nft;//对应不同操作类型的消息体(json)
+    }
+
+    @Data
+    public static class NftV1 {
+        @JSONField(name = "class_id")
+        private String classId;
+
+        @JSONField(name = "nft_id")
+        private String nftId;
+    }
+
+    @Data
+    public static class NftNative {
+        @JSONField(name = "class_id")
+        private String classId;
+
+        @JSONField(name = "id")
+        private String Id;
+    }
+
+    @Data
+    public static class NftEVM {
+        @JSONField(name = "class_id")
+        private String classId;
+
+        @JSONField(name = "id")
+        private Integer Id;
+    }
+
+    @Data
+    public static class Mt {
+        @JSONField(name = "class_id")
+        private String classId;
+
+        @JSONField(name = "mt_id")
+        private String mtId;
+    }
+
+    @Data
+    public static class Record {
+        @JSONField(name = "record_id")
+        private String recordId;// 区块链存证 ID
+
+        @JSONField(name = "certificate_url")
+        private String certificateUrl;// 区块链存证证书的下载链接；证书下载链接并非长期有效，请您尽快将证书文件下载至本地并妥善保管。
+    }
+
+    @Data
+    public static class Kind {
+        @JSONField(name = "kind")
+        private String kind;//区分服务，native / evm
+    }
+}
